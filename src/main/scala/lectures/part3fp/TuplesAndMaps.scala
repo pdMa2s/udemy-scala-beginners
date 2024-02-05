068d6d094fb7fbedc8b757970e0ee381ca7ab082
Pedro Matos <ma2s.pedro@gmail.com>
2024-02-05 22:49:01 +0100

    Tuples and maps exercices

--- src/main/scala/lectures/part3fp/TuplesAndMaps.scala
+++ src/main/scala/lectures/part3fp/TuplesAndMaps.scala
@@ -12,7 +12,7 @@ object TuplesAndMaps extends App {
 
   // Maps - keys -> values
   val aMap: Map[String, Int] = Map()
-  val phonebook = Map(("Jim", 555), "Daniel" -> 789).withDefaultValue(-1)
+  val phonebook = Map(("Jim", 555), "Daniel" -> 789, "JIM" -> 999).withDefaultValue(-1)
 
   // a -> b is sugar for (a, b)
   println(phonebook)
@@ -44,5 +44,95 @@ object TuplesAndMaps extends App {
 
   val names = List("Bob", "James", "Angela", "Mary", "Daniel", "Jim")
   println(names.groupBy(name => name.charAt(0)))
+
+  def add(network: Map[String, Set[String]], person: String): Map[String, Set[String]] =
+    network + (person -> Set())
+
+  def friend(network: Map[String, Set[String]], personA: String, personB: String): Map[String, Set[String]] = {
+    val friendsA = network(personA)
+    val friendsB = network(personB)
+
+    network + (personA -> (friendsA + personB)) + (personB -> (friendsB + personA))
+  }
+
+  def unfriend(network: Map[String, Set[String]], personA: String, personB: String): Map[String, Set[String]] = {
+    val friendsA = network(personA)
+    val friendsB = network(personB)
+
+    network + (personA -> (friendsA - personB)) + (personB -> (friendsB - personA))
+  }
+
+  def remove(network: Map[String, Set[String]], person: String): Map[String, Set[String]] = {
+    def removeAux(friends: Set[String], networkAcc: Map[String, Set[String]]): Map[String, Set[String]] = {
+      if (friends.isEmpty) networkAcc
+      else removeAux(friends.tail, unfriend(networkAcc, person, friends.head))
+    }
+
+    val unfriended = removeAux(network(person), network)
+    unfriended - person
+  }
+
+  val empty: Map[String, Set[String]] = Map()
+  val network = add(add(empty, "Bob"), "Mary")
+  println(network)
+  println(friend(network, "Bob", "Mary"))
+  println(unfriend(friend(network, "Bob", "Mary"), "Bob", "Mary"))
+  println(remove(friend(network, "Bob", "Mary"), "Bob"))
+
+  val people = add(add(add(empty, "Bob"), "Jim"), "Mary")
+  val jimBob = friend(people, "Bob", "Jim")
+  val testNet = friend(jimBob, "Bob", "Mary")
+
+  println(people)
+  println(jimBob)
+  println(testNet)
+
+  def nFriends(network: Map[String, Set[String]], person: String): Int =
+    network.getOrElse(person, Set()).size
+
+  println(nFriends(testNet, "Bob"))
+
+  def personWithMostFriends(network: Map[String, Set[String]]): String = {
+    def mostFriends(peopleSet: Set[String], mostPopular: String): String = {
+      if (peopleSet.isEmpty) mostPopular
+      else {
+        val friendsMostPopular = nFriends(network, mostPopular)
+        val friendsOther = nFriends(network, peopleSet.head)
+        if (friendsMostPopular > friendsOther) mostFriends(peopleSet.tail, mostPopular)
+        else mostFriends(peopleSet.tail, peopleSet.head)
+      }
+    }
+    mostFriends(network.keySet.tail, network.keySet.head)
+  }
+
+  def mostFriends2(network: Map[String, Set[String]]): String =
+    network.maxBy(pair => pair._2.size)._1
+
+  println(personWithMostFriends(testNet))
+  println(mostFriends2(testNet))
+
+
+  def nPeopleWithNoFriends(network: Map[String, Set[String]]): Int = {
+    network.count(_._2.isEmpty)
+  }
+
+  println(nPeopleWithNoFriends(testNet))
+
+
+  def socialConnection(network: Map[String, Set[String]], personA: String, personB: String): Boolean = {
+    def bfs(target: String, consideredPeople: Set[String], discoveredPeople: Set[String]): Boolean = {
+      if (discoveredPeople.isEmpty) false
+      else {
+        val person = discoveredPeople.head
+        if (person == target) true
+        else if (consideredPeople.contains(person)) bfs(target, consideredPeople, discoveredPeople.tail)
+        else bfs(target, consideredPeople + person, discoveredPeople.tail ++ network(person))
+      }
+    }
+    bfs(personB, Set(), network(personA) + personA)
+  }
+
+  println(socialConnection(testNet, "Mary", "Jim"))
+  println(socialConnection(network, "Mary", "Bob"))
 
 }
